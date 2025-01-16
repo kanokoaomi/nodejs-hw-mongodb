d439bad307fb2fcb86916eaaa27185f2e41b48e9
Yelyzaveta Skorohliad <skoroglyadliza@gmail.com>
2025-01-16 20:30:13 +0200

    Fix isValidId function

--- src/middlewares/isValidId.js
+++ src/middlewares/isValidId.js
@@ -2,9 +2,13 @@ import { isValidObjectId } from 'mongoose';
 import createError from 'http-errors';
 
 export const isValidId = (req, res, next) => {
-  const { id } = req.params;
-  if (!isValidObjectId(id)) {
-    return next(createError(404, `${id} is not valid`));
+  const { contactId } = req.params;
+  if (!isValidObjectId(contactId)) {
+    // return next(createError(404, `${id} is not valid`));
+    return res.status(404).json({
+      status: 404,
+      message: `${contactId} is not valid`,
+    });
   }
   next();
 };

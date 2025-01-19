69665a6a9038aa1cd192f72371030a9c928bfae7
Yelyzaveta Skorohliad <skoroglyadliza@gmail.com>
2025-01-19 23:38:00 +0200

    Revert "Revert "Create login route""

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,5 +1,12 @@
 import createHttpError from 'http-errors';
-import { UserCollection } from '../db/models/user';
+import bcrypt from 'bcrypt';
+import { UserCollection } from '../db/models/user.js';
+import { SessionCollection } from '../db/models/session.js';
+import { randomBytes } from 'crypto';
+import {
+  accessTokenLifetime,
+  refreshTokenLifetime,
+} from '../constants/user.js';
 
 export const register = async (payload) => {
   const { email, password } = payload;
@@ -17,3 +24,31 @@ export const register = async (payload) => {
   });
   return newUser;
 };
+
+export const login = async ({ email, password }) => {
+  const user = await UserCollection.findOne({ email });
+  if (!user) {
+    throw createHttpError(401, 'User is not found');
+  }
+
+  const passwordCompare = await bcrypt.compare(password, user.password);
+  if (!passwordCompare) {
+    throw createHttpError(401, 'User is not found');
+  }
+
+  // це для того, щоб видаляти сесію, якщо користувач заходить ще раз з іншого пристрою
+  await SessionCollection.deleteOne({ userId: user._id });
+
+  const accessToken = randomBytes(30).toString('base64');
+  const refreshToken = randomBytes(30).toString('base64');
+
+  await SessionCollection.create({
+    userId: user._id,
+    accessToken,
+    refreshToken,
+    accessTokenValidUntil: Date.now() + accessTokenLifetime,
+    refreshTokenValidUntil: Date.now() + refreshTokenLifetime,
+  });
+};
+
+export const getSession = (filter) => findOne(filter);

96722fbceed304e54e545fdfd39883753d1f097e
Yelyzaveta Skorohliad <skoroglyadliza@gmail.com>
2025-01-27 19:03:33 +0200

    Add swagger documentation for the backend

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -60,7 +60,7 @@ export const postContactController = async (req, res) => {
     if (cloudinaryEnable) {
       photo = await saveFileToCloudinary(req.file);
     } else {
-      photo = await saveFileToUploadsDir(req.file);
+      photo = await saveFileToUploads(req.file);
     }
   }
 
